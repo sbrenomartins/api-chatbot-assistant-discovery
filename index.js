dcbe2ca7b24959136878585c0f1da8fe3319d7a9
Breno M. Silva <sbrenomartins@gmail.com>
2019-04-20 12:57:55 -0300

    adicionado cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = process.env.PORT || 8080;
 
