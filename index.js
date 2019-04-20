1859c99ceba081d29d982516fcb41e20639c17dc
Breno M. Silva <36958762+sbrenomartins@users.noreply.github.com>
2019-04-20 17:26:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')({ origin: true });
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
